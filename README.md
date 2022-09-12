32284c637d0a4d284f53dd365e6dd01e1d44fd7c
A_Cong <xiaojiecong123@sina.com>
2022-09-12 16:34:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 # githubPushTest
+=======
+# githubPushTest
+>>>>>>> 80d90854c81c467b46260bd79a4a80af5cccbda5
+=======
+# githubPushTest
+>>>>>>> 80d90854c81c467b46260bd79a4a80af5cccbda5
---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    qDebug()<<"1111111";
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
